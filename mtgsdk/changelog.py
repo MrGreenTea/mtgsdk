bad649fe60218711af3f8c59545b1940a1b5d01b
jonas <jonas@jonas-netqa.de>
2016-11-30 08:35:12 +0100

    added Version class for changelog

--- mtgsdk/changelog.py
+++ mtgsdk/changelog.py
@@ -1,15 +1,28 @@
+import attr
 from requests import get
 
+from mtgsdk import utils
+
 __URL = 'https://api.magicthegathering.io/v1/changelogs'
+
+
+@attr.s
+class Version:
+    details = attr.ib()
+    id = attr.ib()
+    release_date = attr.ib()
+    version = attr.ib()
 
 
 def full_changelog():
     resp = get(__URL)
     resp.raise_for_status()
-    return resp.json()['changelogs']
+    return map(utils.object_from_dict(Version), resp.json()['changelogs'])
 
 
 def newest_version():
-    return full_changelog()[0]
+    resp = get(__URL + '?pageSize=1')
+    resp.raise_for_status()
+    return Version(**resp.json()['changelog'][0])
 
 
