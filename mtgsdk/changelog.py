3b9326f57725be4f566d59ebbf829978c7f5c65f
MrGreenTea <mrlordalfred@gmail.com>
2017-03-20 21:48:58 +0100

    added filter to remove empty split parts

--- mtgsdk/changelog.py
+++ mtgsdk/changelog.py
@@ -18,8 +18,10 @@ def full_changelog() -> Iterator[Version]:
     resp = get(__URL)
     resp.raise_for_status()
     pattern = re.compile(r'\([0-9]{4}-[0-9]{2}-[0-9]{2}\)')
-    for _v in resp.text.split('##'):
+    for _v in filter(None, resp.text.split('##')):  # filter empty splits (first one)
         match = pattern.search(_v)
+        if match is None:
+            continue
         version = _v[:match.end()].strip()
         release_date = _v[match.start()+1:match.start()-1]
         details = _v[match.end():].strip()
